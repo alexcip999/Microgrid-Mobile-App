24221ab231b014fdb201af18fbd8d767f3d68a4a
alexcip999 <constantinalexandru123@gmail.com>
2026-03-18 15:03:07 +0200

    feat: add custom theme and colors

--- app/src/main/java/ro/utcluj/dsrl/microgridmobileapp/ui/theme/Color.kt
+++ app/src/main/java/ro/utcluj/dsrl/microgridmobileapp/ui/theme/Color.kt
@@ -2,10 +2,41 @@ package ro.utcluj.dsrl.microgridmobileapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val primaryDark = Color(0xFF00E676)
+val onPrimaryDark = Color(0xFF00391C)
+val primaryContainerDark = Color(0xFF00522B)
+val onPrimaryContainerDark = Color(0xFFA7FFC4)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val secondaryDark = Color(0xFF2196F3)
+val onSecondaryDark = Color(0xFFFFFFFF)
+
+val tertiaryDark = Color(0xFFFFA726)
+val onTertiaryDark = Color(0xFF492900)
+
+val backgroundDark = Color(0xFF0F1216)
+val surfaceDark = Color(0xFF171B21)
+val onSurfaceDark = Color(0xFFFFFFFF)
+val onSurfaceVariantDark = Color(0xFF8B949E)
+val outlineDark = Color(0xFF3F444D)
+
+val primaryLight = Color(0xFF006D3B)
+val onPrimaryLight = Color(0xFFFFFFFF)
+val primaryContainerLight = Color(0xFFA7FFC4)
+val onPrimaryContainerLight = Color(0xFF00210E)
+
+val secondaryLight = Color(0xFF0061A4)
+val onSecondaryLight = Color(0xFFFFFFFF)
+
+val tertiaryLight = Color(0xFF8B5000)
+val onTertiaryLight = Color(0xFFFFFFFF)
+
+val backgroundLight = Color(0xFFF8F9FA)
+val surfaceLight = Color(0xFFFFFFFF)
+val onSurfaceLight = Color(0xFF171D1A)
+val onSurfaceVariantLight = Color(0xFF44474E)
+val outlineLight = Color(0xFF707974)
+
+val errorLight = Color(0xFFBA1A1A)
+val onErrorLight = Color(0xFFFFFFFF)
+val errorDark = Color(0xFFFFB4AB)
+val onErrorDark = Color(0xFF690005)
